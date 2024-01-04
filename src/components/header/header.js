62d698f0a2f84e4f61a1aded464de6c03652af6c
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-04 17:51:27 +0900

    :heavy_plus_sign: folder 페이지 생성  , prop-Types 재 설치

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Logo from '../../assets/images/index/logo.svg';
 import useProfileState from '../../hooks/useProfileState';
 import HeaderNavProfile from './headerNavProfile';
+import './header.css';
+import { Link, useLocation } from 'react-router-dom';
+
 function Header() {
   const { profile } = useProfileState();
+  const location = useLocation();
+  const isFolder = location.pathname === '/folder';
 
   return (
     <header>
-      <nav>
-        <a href="index.html">
+      <nav className={`header_nav ${isFolder ? '' : 'isPositionFixed'}`}>
+        <Link to="/">
           <img src={Logo} alt="홈으로 연결된 Linkbrary 로고" />
-        </a>
+        </Link>
         <HeaderNavProfile profile={profile} />
       </nav>
     </header>
