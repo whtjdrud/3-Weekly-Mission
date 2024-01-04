4eb5152353ffee63679a8f7c699cfe691a7d1b56
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-04 20:48:42 +0900

    :heavy_plus_sign:  'styled-components' 적용, addLink Component 분리

--- src/pages/folder.js
+++ src/pages/folder.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import Header from '../components/header/header.js';
 import Footer from '../components/footer/footer.js';
-import MainCard from '../components/index/mainCard';
-import useFolderState from '../hooks/useFolderState';
 import searchIcon from '../assets/images/index/Search.svg';
+import AddLink from '../components/folder/addLink';
 function Folder() {
-  const { folderData } = useFolderState();
-
   return (
-    <div>
+    <>
       <Header />
-
-      <div className="hero-header resetTopmargin">
-        <div className="search-bar">
-          <input type="search" placeholder="링크를 추가해 보세요" />
-          <button>검색</button>
-        </div>
-      </div>
+      <AddLink />
 
       <article className="main_article">
         <div className="search_bar">
@@ -24,15 +15,9 @@ function Folder() {
             <input type="search" id="search-bar_input" name="search-bar" placeholder="링크를 검색해 보세요." style={searchBarStyle} />
           </form>
         </div>
-
-        <div className="grid-container">
-          {folderData.folder.links.map(link => (
-            <MainCard key={link.id} link={link} />
-          ))}
-        </div>
       </article>
       <Footer />
-    </div>
+    </>
   );
 }
 
