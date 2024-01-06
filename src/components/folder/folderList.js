177099fd5659a90ca27a176599d1f452d2a215b2
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-07 01:09:34 +0900

    :art: folderList.js 폴더 버튼 렌더링 분리

--- src/components/folder/folderList.js
+++ src/components/folder/folderList.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { FolderButton, StyledFolderList } from './style/folderList.style';
 
 const FolderList = ({ folderList, activeFolderId, onFolderClick }) => {
+  const renderFolderButton = folder => (
+    <FolderButton key={folder.id} data-id={folder.id} active={folder.id === activeFolderId}>
+      {folder.name}
+    </FolderButton>
+  );
+
   return (
     <StyledFolderList onClick={onFolderClick}>
       <FolderButton data-id="all">전체</FolderButton>
-      {folderList.map(folder => (
-        <FolderButton key={folder.id} data-id={folder.id} active={folder.id === activeFolderId}>
-          {folder.name}
-        </FolderButton>
-      ))}
+      {folderList.map(renderFolderButton)}
     </StyledFolderList>
   );
 };
