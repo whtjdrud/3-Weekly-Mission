ab73a5c2e9014af2a7a1e5805d7b23fb98b9c867
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-06 11:30:52 +0900

    :recycle: mainHeader 스타일 코드 분리

---
+++ src/components/folder/mainHeader.style.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const FolderLinkHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 24px;
+  margin-bottom: 24px;
+`;
+export const HeaderButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 12px;
+`;
+export const HeaderTitle = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const MainTitle = styled.div`
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+export const MainNav = styled.div`
+  display: flex;
+  width: 1060px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const FolderAdd = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  gap: 4px;
+`;
+
+export const FolderAddIcon = styled.img`
+  width: 16px;
+  height: 16px;
+`;
