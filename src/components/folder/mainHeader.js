ebabb855e5a73ec04c40df1102b317bf24479f2f
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-01-06 11:30:52 +0900

    :recycle: mainHeader 스타일 코드 분리

--- src/components/folder/mainHeader.js
+++ src/components/folder/mainHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import addIcon from '../../assets/images/add.svg';
 import { Link } from 'react-router-dom';
 import MainIconButton from './mainIconButton';
 import FolderList from './folderList';
+import { FolderAdd, FolderAddIcon, FolderLinkHeader, HeaderButtons, HeaderTitle, MainNav, MainTitle } from './mainHeader.style';
 
 function MainHeader({ folderList, activeFolderId, onFolderClick }) {
   const activeFolderName = folderList.find(folder => folder.id === activeFolderId)?.name;
@@ -45,54 +45,6 @@ function MainHeader({ folderList, activeFolderId, onFolderClick }) {
   );
 }
 
-const FolderLinkHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  gap: 24px;
-  margin-bottom: 24px;
-`;
-const HeaderButtons = styled.div`
-  display: flex;
-  justify-content: space-between;
-  gap: 12px;
-`;
-const HeaderTitle = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-`;
-
-const MainTitle = styled.div`
-  font-size: 24px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-`;
-
-const MainNav = styled.div`
-  display: flex;
-  width: 1060px;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const FolderAdd = styled.div`
-  display: flex;
-  align-items: center;
-  text-align: center;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-  gap: 4px;
-`;
-
-const FolderAddIcon = styled.img`
-  width: 16px;
-  height: 16px;
-`;
 MainHeader.propTypes = {
   folderList: PropTypes.arrayOf(
     PropTypes.shape({
