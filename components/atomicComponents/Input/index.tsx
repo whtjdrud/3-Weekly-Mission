8a8da29b339d4e9d70918e4a6e617e262a6c24f0
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-02-18 20:09:39 +0900

    :sparkles: 공용 input, passswordInput 추가

---
+++ components/atomicComponents/Input/index.tsx
@@ -0,0 +1,41 @@
+import React, { ChangeEventHandler } from 'react'
+import styles from './input.module.css'
+
+interface InputProps {
+  type: string
+  value: string
+  label: string
+  name: string
+  onChange: ChangeEventHandler<HTMLInputElement>
+  placeholder: string
+  error: string
+}
+const Input: React.FC<InputProps> = ({
+  type,
+  value,
+  name,
+  label,
+  onChange,
+  placeholder = '',
+  error = '',
+}) => {
+  const inputClassName = error
+    ? `${styles.input} ${styles.input_error}`
+    : styles.input
+
+  return (
+    <div className={styles.sign_box_input}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={inputClassName}
+      />
+      <p className="error_msg">{error}</p>
+    </div>
+  )
+}
+export default Input
