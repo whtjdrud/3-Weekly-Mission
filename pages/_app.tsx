4a94e7f0bb84642d1a22d159158962615af9c641
whtjdrud <151587265+whtjdrud@users.noreply.github.com>
2024-02-19 01:58:23 +0900

    :sparkles: 페이지 입장 불가 기능

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import MainHead from '@/components/mainhead'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem('accessToken')
+
+    const isAuthPage =
+      router.pathname === '/login' || router.pathname === '/signup'
+
+    if (isAuthPage && accessToken) {
+      router.push('/folder')
+    }
+    if (!isAuthPage && !accessToken) {
+      router.push('/login')
+    }
+  }, [router])
+
   return (
     <>
       <MainHead />
