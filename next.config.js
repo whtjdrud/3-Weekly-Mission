3ee901e4a55f2a42a8868e364fd4f77595e90ff1
jobible <whtjdrud800@gmail.com>
2024-02-26 17:15:20 +0900

    :wrench: 이미지 domain 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'], // 이미지 호스트 이름 추가
+    domains: ['avatars.githubusercontent.com', 'cloudflare-ipfs.com'],
   },
   reactStrictMode: true,
 }
