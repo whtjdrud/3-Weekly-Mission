3bc04bec145689f71de7ab882821d9ced49b45c4
jobible <whtjdrud800@gmail.com>
2024-02-29 08:59:51 +0900

    :fire: 사용하지 않는 useState 삭제

--- pages/shared/[folderid].tsx
+++ pages/shared/[folderid].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HeaderPage from '@/components/Header'
 import Footer from '@/components/Footer'
 import SearchBar from '@/components/SearchBar'
